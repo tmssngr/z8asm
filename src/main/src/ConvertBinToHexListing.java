d1c99993f5d70fb25ed6fe90b5a876c4bb02878d
Thomas Singer <tmssngr@users.noreply.github.com>
2023-03-20 15:39:28 +0100

    ConvertBinToHexListing

---
+++ src/main/src/ConvertBinToHexListing.java
@@ -0,0 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import com.syntevo.antlr.z8.Output;
+
+/**
+ * @author Thomas Singer
+ */
+public class ConvertBinToHexListing {
+
+	// Static =================================================================
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.out.println("Need input file");
+			return;
+		}
+
+		final Output output = new Output();
+		try (final InputStream stream = Files.newInputStream(Paths.get(args[0]))) {
+			while (true) {
+				final int data = stream.read();
+				if (data < 0) {
+					break;
+				}
+
+				output.write(data);
+			}
+		}
+
+		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("output.txt"), StandardOpenOption.CREATE_NEW)) {
+			output.print(writer);
+		}
+	}
+}
