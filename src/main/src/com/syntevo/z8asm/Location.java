b1deccc7f6eeb8f484baf2a835cf063b410460b3
Thomas Singer <tmssngr@users.noreply.github.com>
2026-08-14 17:07:58 +0200

    Location: @NotNull

--- src/main/src/com/syntevo/z8asm/Location.java
+++ src/main/src/com/syntevo/z8asm/Location.java
@@ -1,5 +1,7 @@
 package com.syntevo.z8asm;
 
+import org.jetbrains.annotations.*;
+
 /**
  * @author Thomas Singer
  */
@@ -7,6 +9,7 @@ public record Location(int line, int column) {
 
 	public static final Location DUMMY = new Location(-1, -1);
 
+	@NotNull
 	@Override
 	public String toString() {
 		return (line + 1) + ":" + (column + 1);
